f925823fbdabf480f7b5d60b8bafc3d5cd841aa9
Aida Ghayour <aghayourkhia@mun.ca>
2019-12-21 10:40:32 -0330

    solved Cmake's openssl call

--- VG.sh
+++ VG.sh
@@ -11,6 +11,14 @@ sudo apt-get install build-essential git cmake pkg-config libncurses-dev libbz2-
                      libffi-dev libcairo-dev
 
 
-################################# make sure you have installed Cmake from here: https://github.com/Kitware/CMake/releases/download/v3.16.2/cmake-3.16.2.tar.gz
+################################# make sure you have installed Cmake installed which links to openssl's curl libraries. This can be troublesome but this would work:
+#cd /tmp
+#sudo apt-get install libcurl4-openssl-dev
+#wget https://github.com/Kitware/CMake/releases/download/v3.14.4/cmake-3.14.4.tar.gz
+#tar -xf cmake-3.14.4.tar.gz
+#cd cmake-3.14.4
+#./bootstrap --system-curl --parallel=16 # Use `parallel` argument to accelerate the process
+#make -j16 # Use `j` argument to accelerate the process
+#sudo make install
 
 ./source_me.sh && make
